6b83a36530f3f29fac9f85556bc4b1be8ef947a5
Holly Maxwell <hollymaxwell@Hollys-MacBook-Pro.local>
2018-12-09 18:00:12 -0800

    Committing completed JSON practice assignment

---
+++ jsonDEMO/JS/json_weather.js
@@ -0,0 +1,24 @@
+// 9 Oct 2014 seems like it should be 19 Oct 2014 but the txt file says 9 Oct 2014, typo maybe? I've changed it in my code
+
+var data = {"created" : "2014-10-17T14:42:39Z",
+    "lang" : "en-US",
+    "location" :
+            {"city" : "Orlando", "country" : "United States", "region" : "FL"},
+    "results" :
+        {"units" :
+            {"distance" : "mi", "pressure" : "ins", "speed" : "mph", "temperature" : "F"},
+        "wind" :
+            {"chill" : 76, "direction" : 360, "speed" : 5},
+        "atmosphere" :
+            {"humidity" : 48, "pressure" : 29.65, "rising" : 0, "visibility" : 10},
+        "astronomy" :
+            {"sunrise" : "7:26 am", "sunset" : "6:51 pm"},
+        "condition" :
+            {"code" : 33, "date" : "Fri, 17 Oct 2014 5:53 am EST", "temp" : 76, "text" : "Fair"},
+        "forecast" : [
+            {"code": 30, "date":"17 Oct 2014", "day" : "Friday", "high": 80, "low": 62, "text": "Sunny"},
+            {"code": 30, "date":"18 Oct 2014", "day" : "Saturday", "high" : 82, "low": 63, "text": "Partly Cloudy"},
+            {"code": 30, "date":"19 Oct 2014", "day" : "Sunday", "high" : 85, "low": 65, "text": "Partly Cloudy"}]}};
+
+
+
